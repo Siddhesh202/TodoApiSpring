7f919841627ae063c4b6f4f817e97423f0b65b1a
Siddhesh Pisal <siddheshpisal76@gmail.com>
2024-03-31 12:10:22 +0530

    Added custom time monitor annotation

--- src/main/java/com/example/todoapispring/FakeTodoService.java
+++ src/main/java/com/example/todoapispring/FakeTodoService.java
@@ -4,8 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service("fakeTodoService")
 public class FakeTodoService implements TodoService{
+
+    @TimeMonitor
     @Override
     public String doSomething() {
+        for(int i = 0; i < 1000000000; i++){
+
+        }
         return "Something from fake todo service";
-    }
+    } // Join Point
 }
